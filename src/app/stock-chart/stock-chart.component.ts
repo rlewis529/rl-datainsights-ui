447ecb8ef292375f518d95ef2afa3820a634994d
Robert Lewis <rlewis529@gmail.com>
2025-04-22 13:21:37 -0400

    Adding begin and end dates

--- src/app/stock-chart/stock-chart.component.ts
+++ src/app/stock-chart/stock-chart.component.ts
@@ -12,10 +12,25 @@ import { RouterModule } from '@angular/router';
 })
 export class StockChartComponent {
   ticker = '';
+  startDate: string | null = null;
+  endDate: string | null = null;
   chartUrl: string | null = null;
 
   getChart() {
     const timestamp = new Date().getTime();
-    this.chartUrl = `http://localhost:5000/api/stock-chart?ticker=${this.ticker}&_=${timestamp}`;
+    let url = `http://localhost:5000/api/stock-chart?ticker=${this.ticker}`;
+
+    if (this.startDate) {
+      url += `&start=${this.startDate}`;
+    }
+
+    if (this.endDate) {
+      url += `&end=${this.endDate}`;
+    }
+
+    url += `&_=${timestamp}`; // to avoid browser cache
+
+    this.chartUrl = url;
   }
 }
+
